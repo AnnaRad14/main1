c062547dab6154748b33355b5b1ee1d33b6bd25e
AnnaRad14 <145984789+AnnaRad14@users.noreply.github.com>
2024-11-21 10:19:22 +0200

    Update labSQL1.sql

--- labSQL1.sql
+++ labSQL1.sql
@@ -7,10 +7,10 @@ CREATE TABLE my_table (
 ALTER TABLE my_table ADD COLUMN age INT;
 
 INSERT INTO my_table (name, age) VALUES 
-('Alice', 25),
-('Bob', 30),
-('Charlie', 22),
-('David', 35);
+('A', 25),
+('B', 30),
+('C', 22),
+('D', 35);
 
 SELECT * FROM my_table;
 SELECT * FROM my_table WHERE age >= 25;
